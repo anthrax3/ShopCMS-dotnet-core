cbc854e165416ec7de8b8c9f1efaa3b39431c4d0
Will <willj@users.noreply.github.com>
2018-03-31 13:42:50 +0100

    initial commit

---
+++ app/ShopCms/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace ShopCms.Models
+{
+    public class Product
+    {
+        public int ID { get; set; }
+        public string Slug { get; set; }
+        public string Title { get; set; }
+        public string Desc { get; set; }
+        public string Image { get; set; }
+        public List<Category> Categories { get; set; }
+        public List<Variant> Variants { get; set; }
+    }
+}
---
+++ app/ShopCms/Models/Variant.cs
@@ -0,0 +1,10 @@
+﻿namespace ShopCms.Models
+{
+    public class Variant
+    {
+        public string ID { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Image { get; set; }
+    }
+}
